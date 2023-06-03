f64cc5808660eae0b1ab013e59e95ade8d12a61d
Marshall Burns <marshallbcodes@gmail.com>
2023-06-02 22:37:56 -0500

    Start adding icons/minor changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,80 @@
 import React from 'react'
+import marshall from '../src/assets/images/grayscaleMarshall.jpg'
+import js from '../src/assets/images/techstackIcons/javascript.svg'
+import ts from '../src/assets/images/techstackIcons/typescript.svg'
+// import react from '../src/assets/images/techstackIcons/react.svg' TODO: add react icon
+import java from '../src/assets/images/techstackIcons/java.svg'
+import python from '../src/assets/images/techstackIcons/python.svg'
+// import node from '../src/assets/images/techstackIcons/node.svg' TODO: add node icon
+import express from '../src/assets/images/techstackIcons/express.svg'
+// import mongodb from '../src/assets/images/techstackIcons/mongodb.svg' TODO: add mongodb icon
+import git from '../src/assets/images/techstackIcons/git.svg'
+// import github from '../src/assets/images/techstackIcons/github.svg' TODO: add github icon
+
+// import gitlab from '../src/assets/images/techstackIcons/gitlab.svg' TODO: add gitlab icon
+import bitbucket from '../src/assets/images/techstackIcons/bitbucket.svg'
+import netlify from '../src/assets/images/techstackIcons/netlify.svg'
+import postman from '../src/assets/images/techstackIcons/postman.svg'
 
 export default function App() {
   return (
     <>
-      {/* about me section */}
+      {/* start of about me section */}
       <div className='aboutMeContainer'>
-        <div className='aboutMeParaContainer'>{/* TODO: add paragraph */}</div>
+        <div className='aboutMeParaContainer'>
+          <h4 className='aboutMeParaHeading'>Who is Marshall B?</h4>
+          <p>
+            A passionate Software Developer from Chicago, Illinois with a love
+            for coding and providing solutions to the worlds problems through
+            software. Experienced in JavaScript, TypeScript, React, MongoDB and
+            so much more!
+          </p>
+        </div>
 
-        <div className='aboutMeImgContainer'>{/* TODO: add img */}</div>
+        <div className='aboutMeImgContainer'>
+          <img src={marshall} alt='Marshall B' />
+        </div>
       </div>
+      {/* end of about me section */}
 
-      {/* techstack section */}
+      {/* start of techstack section */}
       <div className='techStackContainer'>
-        <div className='frontendStackContianer'>
-          {/* TODO: add frontend tech */}
+        {/* icon container */}
+        <div className='iconContainer'>
+          <h4 className='techstackHeading'>The Stack</h4>
+          <div className='icons'>
+            {/* icon images */}
+            <img src={js} alt='JavaScript' />
+          </div>
+          <div className='icons' id='typescriptIcon'>
+            <img src={ts} alt='TypeScript' />
+          </div>
+          <div className='icons' id='javaIcon'>
+            <img src={java} alt='Java' />
+          </div>
+          <div className='icons' id='pythonIcon'>
+            <img src={python} alt='Python' />
+          </div>
+          <div className='icons' id='expressIcon'>
+            <img src={express} alt='Express' />
+          </div>
+          <div className='icons' id='gitIcon'>
+            <img src={git} alt='Git' />
+          </div>
+          <div className='icons' id='bitbucketIcon'>
+            <img src={bitbucket} alt='Bitbucket' />
+          </div>
+          <div className='icons' id='netlifyIcon'>
+            <img src={netlify} alt='Netlify' />
+          </div>
+          <div className='icons' id='postmanIcon'>
+            <img src={postman} alt='Postman' />
+          </div>
         </div>
-
-        <div className='backendStackContainer'>
-          {/* TODO: add backend tech */}
-        </div>
-
-        <div className='otherStackContainer'>{/* TODO: add other tech */}</div>
       </div>
+      {/* end of techstack section */}
 
-      {/*projects section  */}
+      {/*start of projects section  */}
       <div className='projectsContainer'>
         <div className='project1Container'>
           {/* TODO: add international data matrix info */}
@@ -31,6 +82,7 @@ export default function App() {
 
         <div className='project2Container'>{/* TODO: add husk info */}</div>
       </div>
+      {/* end of projects section */}
     </>
   )
 }

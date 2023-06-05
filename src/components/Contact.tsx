069d0dc06e69a0ab68822c520b997616115d815f
Marshall Burns <marshallbcodes@gmail.com>
2023-06-05 07:10:21 -0500

    Styling changes/add assets/update contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,19 +1,56 @@
-
 export default function Contact() {
   return (
     <>
       <div className='contactContainer'>
-        <h4 className='contactHeading headings'>Contact Me</h4>
-        <form className='contactForm'>
-          <label htmlFor='name'>Name</label>
-          <input type='text' id='name' name='name' placeholder='Your name..' />
-          <label htmlFor='email'>Email</label>
-          <input type='email' id='email' name='email' placeholder='Your email..' />
-          <label htmlFor='subject'>Subject</label>
-          <textarea className='textArea' id='subject' name='subject' placeholder='What does this concern?'></textarea>
-          <textarea id='textArea' name='description' placeholder='Write something...'></textarea>
-        </form>
-    </div>
+        <h4 className='contactHeading'>Contact Me</h4>
+        <div className='contactFormContainer'>
+          <form
+            className='contactForm'
+            action='https://formspree.io/f/mknelldn'
+            method='POST'
+          >
+            <label htmlFor='name'></label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              placeholder='Your name...'
+            />
+            <label htmlFor='email'></label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              placeholder='Your email...'
+            />
+            <label htmlFor='subject'></label>
+            <textarea
+              className='textArea subject '
+              maxLength={20}
+              id='subject'
+              name='subject'
+              placeholder='What does this concern?'
+            ></textarea>
+            <textarea
+              className='textArea description'
+              name='description'
+              placeholder='Your Message'
+              rows={10}
+              maxLength={400}
+            ></textarea>
+            <button
+              className='contactButton'
+              title='submit'
+              role='button'
+              type='submit'
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+
+        <div className='contactLinks'></div>
+      </div>
     </>
   )
 }
